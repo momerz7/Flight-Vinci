5e757d5f1fd6a19a58d7e829547a3eb9ac5666fd
{momerz7} <{didicr7@icloud.com}>
2025-12-17 21:52:31 -0500

    backend structure

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from app.routers import health
 
 app = FastAPI()
+app.include_router(health.router)
 
 @app.get("/")
 def root():
     return {"message": "FlightVinci backend is running"}
+
+load_dotenv()
